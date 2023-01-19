4e5704fd4e763a62ba7ad6047998b74b2da6eee2
1 <kudakovazam@yandex.ru>
2023-01-19 20:24:05 +0300

    master

--- women/templatetags/women_tags.py
+++ women/templatetags/women_tags.py
@@ -3,6 +3,14 @@ from women.models import *
 
 register = template.Library()
 
-@register.simple_tag()
-def get_categories():
-    return Category.objects.all()
+@register.simple_tag(name='getcats')
+def get_categories(filter=None):
+    if not filter:
+        return Category.objects.all()
+    else:
+        return Category.objects.filter(pk=filter)
+
+@register.inclusion_tag('women/list_categories.html')
+def show_categories(cat_selected=0):
+    cats = Category.objects.all()
+    return {"cats": cats, 'cat_selected': cat_selected}
