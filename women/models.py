c0770f8dd3ae976e8afc371019e33ea40b51d556
1 <kudakovazam@yandex.ru>
2023-01-21 20:25:03 +0300

    master

--- women/models.py
+++ women/models.py
@@ -4,12 +4,13 @@ from django.urls import reverse
 
 class Women(models.Model):
     title = models.CharField(max_length=255, verbose_name='Заголовок')
+    slug = models.SlugField(max_length=255, unique=True, db_index=True, verbose_name='URL')
     content = models.TextField(blank=True)
     photo = models.ImageField(upload_to="photos/%Y/%m/%d/")
     time_create = models.DateTimeField(auto_now_add=True)
     time_update = models.DateTimeField(auto_now=True)
     is_published = models.BooleanField(default=True)
-    cat = models.ForeignKey('Category', on_delete=models.PROTECT, null=True)
+    cat = models.ForeignKey('Category', on_delete=models.PROTECT,)
 
     def __str__(self):
         return self.title
@@ -26,6 +27,7 @@ class Women(models.Model):
 
 class Category(models.Model):
     name = models.CharField(max_length=100, db_index=True, verbose_name='Категория')
+    slug = models.SlugField(max_length=255, unique=True, db_index=True, verbose_name='URL')
 
     def __str__(self):
         return self.name
--- women/views.py
+++ women/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse, HttpResponseNotFound, Http404
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 
 from .models import *
 
@@ -11,14 +11,14 @@ menu = [{'title': "about site", "url_name": "about"},
 
 def index(request):
     posts = Women.objects.all()
-    cats = Category.objects.all()
+
     context = {
         'posts': posts,
-        'cats': cats,
         'menu': menu,
         'title': 'Главная страница',
         'cat_selected' : 0,
     }
+
     return render(request, 'women/index.html', context=context)
 
 def about(request):
@@ -39,18 +39,25 @@ def login(requsest):
     return HttpResponse('Authorization')
 
 def show_post(request, post_id):
-    return HttpResponse(f"Post id --------- {post_id}")
+    post = get_object_or_404(Women, pk=post_id)
+
+    context = {
+        'post': post,
+        'menu': menu,
+        'title': post.title,
+        'cat_selected': post.cat_id,
+    }
+
+    return render(request, "women/post.html", context=context)
 
 def show_category(request, cat_id):
     posts = Women.objects.filter(cat_id=cat_id)
-    cats = Category.objects.all()
 
     if len(posts) == 0:
         raise Http404()
 
     context = {
         'posts': posts,
-        'cats': cats,
         'menu': menu,
         'title': 'Categorys',
         'cat_selected': cat_id,
